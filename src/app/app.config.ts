74c1ddb429614033241cf68caace8a4e81d3ffed
Andy Radulescu <andy@Andys-MacBook-Pro.local>
2024-03-04 18:22:41 +0200

    init app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,32 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { routes } from './app.routes';
 
+const initialize = ()=>{
+const firebaseConfig = {
+  apiKey: "AIzaSyDUxOuD0l6dDE4qT-ullBT9xCZY4spbm5w",
+  authDomain: "wedding-website-6365a.firebaseapp.com",
+  projectId: "wedding-website-6365a",
+  storageBucket: "wedding-website-6365a.appspot.com",
+  messagingSenderId: "978884966792",
+  appId: "1:978884966792:web:c271201ea166363d7e223d",
+  measurementId: "G-CXD39TTK9R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+}
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    {
+      provide:APP_INITIALIZER,
+      useFactory:initialize,
+      multi:true,
+    }
+  ]
 };
