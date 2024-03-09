8a304d6af8ce77c8898c0a26a3728e6b5bb83537
andyRadulescu <radulescu.eduard.andrei@gmail.com>
2024-03-09 18:28:22 +0200

    start hero-section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeroSectionComponent} from "./hero-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'wedding-website';
+  title = 'Andy&Diana';
 }
