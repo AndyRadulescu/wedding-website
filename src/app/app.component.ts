9a26733ab6a2f1afe0997dff135dbc78fcf30f67
andyRadulescu <radulescu.eduard.andrei@gmail.com>
2024-03-10 14:39:15 +0200

    add card invitation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeroSectionComponent} from "./sections/hero-section.component";
+import {CardInvitationComponent} from "./sections/card-invitation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroSectionComponent],
+  imports: [RouterOutlet, HeroSectionComponent, CardInvitationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/sections/card-invitation.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-map-location',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './card-invitation.component.html',
+  styleUrl: './card-invitation.component.scss'
+})
+export class CardInvitationComponent {
+
+}
