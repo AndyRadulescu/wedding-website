19321f920559302de23b58b7a8fd9b16d4d35810
andyRadulescu <radulescu.eduard.andrei@gmail.com>
2024-03-10 15:42:26 +0200

    add section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeroSectionComponent} from "./sections/hero-section.component";
 import {CardInvitationComponent} from "./sections/card-invitation.component";
+import {FooterComponent} from "./sections/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroSectionComponent, CardInvitationComponent],
+  imports: [RouterOutlet, HeroSectionComponent, CardInvitationComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
