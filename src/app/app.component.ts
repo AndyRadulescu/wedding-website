9346aceda9be5cf9bd50ac76156d98f98c0a01f4
andyRadulescu <radulescu.eduard.andrei@gmail.com>
2024-03-11 15:53:14 +0200

    add photo section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import {RouterOutlet} from '@angular/router';
 import {HeroSectionComponent} from "./sections/hero-section.component";
 import {CardInvitationComponent} from "./sections/card-invitation.component";
 import {FooterComponent} from "./sections/footer.component";
+import {PhotoSectionComponent} from "./sections/photo-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroSectionComponent, CardInvitationComponent, FooterComponent],
+  imports: [RouterOutlet, HeroSectionComponent, CardInvitationComponent, FooterComponent, PhotoSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
