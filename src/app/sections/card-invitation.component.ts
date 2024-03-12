e41f4eb297dd34958785f9de62eb1720503582b5
andyRadulescu <radulescu.eduard.andrei@gmail.com>
2024-03-12 16:56:50 +0200

    translate the app

--- src/app/sections/card-invitation.component.ts
+++ src/app/sections/card-invitation.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {TranslocoPipe} from "@ngneat/transloco";
 
 @Component({
   selector: 'card-invitation',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    TranslocoPipe
   ],
   templateUrl: './card-invitation.component.html',
   styleUrl: './card-invitation.component.scss'
