98a3ed9d1619a7688ec10fcfe5a9fcdf6e29075a
andyRadulescu <radulescu.eduard.andrei@gmail.com>
2024-03-12 19:43:05 +0200

    add images

--- src/app/sections/photo-section.component.ts
+++ src/app/sections/photo-section.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-photo-section',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './photo-section.component.html',
   styleUrl: './photo-section.component.scss'
 })
