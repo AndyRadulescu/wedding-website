850bf947ef266d3b560bc4c97fb62d45df8cffed
andyRadulescu <radulescu.eduard.andrei@gmail.com>
2024-03-17 17:56:26 +0200

    test time until

--- src/app/utils/time-until.spec.ts
+++ src/app/utils/time-until.spec.ts
@@ -1,7 +1,95 @@
 import {timeUntil} from "./time-until";
 
+
+const weddingDate = new Date(2024, 6, 23, 16, 0, 0, 0);
 describe("Time until", () => {
-  it('should get the time until 23 June 2024', () => {
-    expect(timeUntil(new Date('2024-06-23'))).toEqual(3);
-  })
+  it('should get the time until 23 June 2024 from one moth prior', () => {
+    const date = new Date(2024, 6, 0, 0, 0, 0);
+    jasmine.clock().mockDate(date);
+
+    expect(
+      timeUntil(weddingDate)
+    ).toEqual({days: 23, hours: 16, minutes: 0, seconds: 0});
+  });
+
+  it('should return 0 for equal dates', () => {
+    const date = new Date(2024, 6, 23, 16, 0, 0, 0);
+    jasmine.clock().mockDate(date);
+
+    expect(
+      timeUntil(weddingDate)
+    ).toEqual({days: 0, hours: 0, minutes: 0, seconds: 0});
+  });
+
+  it('should get the time until 23 June 2024 prior one minute', () => {
+    const date = new Date(2024, 6, 23, 15, 59, 0);
+    jasmine.clock().mockDate(date);
+
+    expect(
+      timeUntil(weddingDate)
+    ).toEqual({days: 0, hours: 0, minutes: 1, seconds: 0});
+  });
+
+  it('should get the time until 23 June 2024 prior several seconds', () => {
+    const date = new Date(2024, 6, 23, 15, 59, 27);
+    jasmine.clock().mockDate(date);
+
+    expect(
+      timeUntil(weddingDate)
+    ).toEqual({days: 0, hours: 0, minutes: 0, seconds: 33});
+  });
+
+  it('should get the time until 23 June 2024 prior one hour and several minutes', () => {
+    const date = new Date(2024, 6, 23, 15, 37, 23);
+    jasmine.clock().mockDate(date);
+
+    expect(
+      timeUntil(weddingDate)
+    ).toEqual({days: 0, hours: 0, minutes: 22, seconds: 37});
+  });
+
+  it('should get the time until 23 June 2024 from one moth prior and hours', () => {
+    const date = new Date(2024, 6, 3, 15, 37, 23);
+    jasmine.clock().mockDate(date);
+
+    expect(
+      timeUntil(weddingDate)
+    ).toEqual({days: 20, hours: 0, minutes: 22, seconds: 37});
+  });
+
+  it('should get the time until 23 June 2024 with time before the wedding', () => {
+    const date = new Date(2024, 4, 25, 3, 11, 35);
+    jasmine.clock().mockDate(date);
+
+    expect(
+      timeUntil(weddingDate)
+    ).toEqual({days: 59, hours: 12, minutes: 48, seconds: 25});
+  });
+
+  it('should get the time until 23 June 2024 after one minute', () => {
+    const date = new Date(2024, 6, 24, 16, 1, 0);
+    jasmine.clock().mockDate(date);
+
+    expect(
+      timeUntil(weddingDate)
+    ).toEqual({days: 0, hours: 0, minutes: 0, seconds: 0});
+  });
+
+  it('should get the time until 23 June 2024 after one second', () => {
+    const date = new Date(2024, 6, 24, 16, 0, 1);
+    jasmine.clock().mockDate(date);
+
+    expect(
+      timeUntil(weddingDate)
+    ).toEqual({days: 0, hours: 0, minutes: 0, seconds: 0});
+  });
+
+  it('should get the time after 23 june 2024', () => {
+    const date = new Date(2024, 6, 25, 3, 11, 35);
+    jasmine.clock().mockDate(date);
+
+    expect(
+      timeUntil(weddingDate)
+    ).toEqual({days: 0, hours: 0, minutes: 0, seconds: 0});
+  });
 })
--- src/app/utils/time-until.ts
+++ src/app/utils/time-until.ts
@@ -1,16 +1,19 @@
-import {
-  differenceInDays,
-  differenceInHours,
-  differenceInMilliseconds,
-  differenceInMonths,
-  differenceInSeconds
-} from "date-fns";
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
 
 export const timeUntil = (futureDate: Date) => {
-  const now = Date();
-  const seconds = differenceInSeconds(futureDate, now)
-  const hours = differenceInHours(futureDate, now)
-  const days = differenceInDays(futureDate, now)
-  const months = differenceInMonths(futureDate, now)
-  return days;
+  const now = new Date();
+  const distance = futureDate.getTime() - now.getTime();
+  if (distance < 0) {
+    return {days: 0, hours: 0, minutes: 0, seconds: 0};
+  }
+  const days = Math.floor(distance / DAY);
+  const hours = Math.floor((distance % DAY) / HOUR);
+  const minutes = Math.floor((distance % HOUR) / MINUTE);
+  const seconds = Math.floor((distance % MINUTE) / SECOND);
+
+  return {days, hours, minutes, seconds};
 }
+
