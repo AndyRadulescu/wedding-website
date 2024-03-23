3ea3974f5d5e52717ce8c3b8605507d621928488
andyRadulescu <radulescu.eduard.andrei@gmail.com>
2024-03-23 20:48:02 +0200

    add carousel component

--- src/app/components/slider-container.component.ts
+++ src/app/components/slider-container.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {AdditionalItemComponent} from './additional-item.component';
 import {TranslocoDirective} from '@ngneat/transloco';
+import { NgxTCarouselComponent } from 'ngx-tcarousel';
 
 @Component({
   selector: 'app-slider-container',
   standalone: true,
   imports: [
     AdditionalItemComponent,
-    TranslocoDirective
+    TranslocoDirective,
+    NgxTCarouselComponent
   ],
   templateUrl: './slider-container.component.html',
   styleUrl: './slider-container.component.scss'
